85da826c7ff57ed19f23b2950799f05d468b8efc
Misha <mishagrigoryan@gmail.com>
2023-08-12 21:30:50 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from reddit_service import create_reddit_instance, get_subreddits, search_reddit
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('search.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    reddit = create_reddit_instance()
+    username = request.form.get('username')
+    search_string = request.form.get('search_string')
+    
+    subreddits = get_subreddits(reddit, username)
+    matching_posts, matching_comments = search_reddit(reddit, username, search_string)
+    
+    return render_template('results.html', username=username, subreddits=subreddits, 
+                           matching_posts=matching_posts, matching_comments=matching_comments)
+
+if __name__ == "__main__":
+    app.run(debug=True)
