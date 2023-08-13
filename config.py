adcc2303bcbfef0242ebf14fdac040df005da337
Misha <mishagrigoryan@gmail.com>
2023-08-12 21:30:00 -0400

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Reddit API configuration
+USERNAME = "Searchbot_01"
+PASSWORD = "aaaa1111"
+CLIENT_ID = "Ai32qfXNqvGuMEvHFFMlAw"
+CLIENT_SECRET = "IG5XKjyUGkcG2cgXfBSwVvalMTxFRg"
+USER_AGENT = "Searchbot_01"
+CHECK_FOR_ASYNC = False
