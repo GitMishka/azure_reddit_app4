0eb938e8cbdbab6183c6b3b6cc0ffe9a9ab1298d
Mikhail <mishagrigoryan@gmail.com>
2023-12-17 16:28:45 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
 # config.py
 
-USERNAME = "Searchbot_01"
-PASSWORD = "aaaa1111"
-CLIENT_ID = "Ai32qfXNqvGuMEvHFFMlAw"
-CLIENT_SECRET = "IG5XKjyUGkcG2cgXfBSwVvalMTxFRg"
-USER_AGENT = "Searchbot_01"
-CHECK_FOR_ASYNC = False
